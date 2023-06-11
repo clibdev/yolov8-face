e99f7f49ab4bb05ec13928c2643bdd25ea25fc72
clibdev <clibdev@gmail.com>
2023-06-11 15:44:53 +0300

    use float instead of np.float

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Differences between original repository and fork:
 * Compatibility with PyTorch >=2.0. (🔥)
 * Original pretrained models from GitHub [releases page](https://github.com/clibdev/yolov8-face/releases). (🔥)
 * Sample script [detect.py](detect.py) for inference.
+* The following deprecations has been fixed:
+  * DeprecationWarning: 'np.float' is a deprecated alias for builtin 'float'.
 
 # Installation
 
@@ -22,4 +24,22 @@ pip install -r requirements.txt
 
 ```shell
 python detect.py --weights yolov8n-face.pt --source ultralytics/assets/bus.jpg --save-img
+```
+
+# WIDERFace evaluation
+
+* Download WIDERFace [validation dataset](https://drive.google.com/file/d/1GUCogbp16PMGa39thoMMeWxp7Rp5oM8Q/view).
+* Move dataset to `data/widerface/val` directory.
+
+```shell
+python test_widerface.py --weights yolov8n-face.pt --dataset_folder data/widerface/val/images/
+```
+```shell
+cd widerface_evaluate
+```
+```shell
+python setup.py build_ext --inplace
+```
+```shell
+python evaluation.py
 ```
--- test_widerface.py
+++ test_widerface.py
@@ -4,11 +4,11 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='./yolov8n-face.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', type=str, default='./yolov8n-face.pt', help='model.pt path(s)')
     parser.add_argument('--img-size', nargs= '+', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.01, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--device', type=str, default='cpu', help='augmented inference')
+    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--save_folder', default='./widerface_evaluate/widerface_txt/', type=str, help='Dir to save txt results')
     parser.add_argument('--dataset_folder', default='./data/widerface/val/images/', type=str, help='dataset path')
