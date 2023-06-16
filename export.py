42329afba7e84fb548a1e0330879162f28f47bd1
clibdev <clibdev@gmail.com>
2023-06-16 07:53:46 +0300

    add device arg to export

--- export.py
+++ export.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     parser.add_argument('--weights', type=str, default='./yolov8n-face.pt', help='model.pt path(s)')
     parser.add_argument('--img-size', nargs='+', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--dynamic', action='store_true', default=False, help='enable dynamic axis in onnx model')
+    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     opt = parser.parse_args()
 
     model = YOLO(opt.weights)
@@ -14,5 +15,6 @@ if __name__ == '__main__':
         format='onnx',
         imgsz=opt.img_size,
         dynamic=opt.dynamic,
+        device=opt.device,
         opset=18
     )
